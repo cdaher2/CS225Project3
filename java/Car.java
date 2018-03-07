405840bea5de08ceeb518fbe10bfd831a517227c
Christian Daher <c_daher2@post.massbay.edu>
2018-03-07 11:38:04 -0500

    Added attributes for Car

--- java/Car.java
+++ java/Car.java
@@ -1,7 +1,43 @@
 package RacingSimulator;
 
+import java.io.File;
+
 public class Car {
 
-    
+    int driveWheels;
+    int tireGrip;
+    int engineMaxRPM;
+    int enginePeakTorque;
+    int currentEngineRPM;
+    int currentEngineTorque;
+    int weight;
+    int downforceFromAero;
+    int engineTemp;
+    int damage;
+    int frontSuspensionStiffness;
+    int rearSuspensionStiffness;
+    Driver driver;
+    String makeAndModel;
+    File image, dataFile;
+    double[][] coordinates;
+
+
+    public Car(String f){
+        dataFile = new File(f);
+        readDataFromFile();
+    }
+
+
+    private void readDataFromFile(){
+
+    }
+
+    public double calculateTopSpeedInCurrentConditions(){
+        return 0;
+    }
+
+    public void findBestLine(){
+
+    }
 
 }
