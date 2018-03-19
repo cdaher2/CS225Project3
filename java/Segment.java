6bbc60b3fb3440f66656a0e7a94a5b494dcc32f6
cdaher2 <31801899+cdaher2@users.noreply.github.com>
2018-03-19 19:52:47 +0000

    Copied new Segment from testspace to master

--- java/Segment.java
+++ java/Segment.java
@@ -1,18 +1,29 @@
 package RacingSimulator;
 
 /**
+ * @author Christian Daher
  *
- * @author d_troy, Christian Daher
+ * A basic data class for Segments
+ * Contains the length, width, corner angle, and surface grip of each piece
  */
 public class Segment{
     private int length;
     private int width;
     private int angle;
+    private int grip;
 
-    public Segment(int l, int w, int a){
+    /**
+     * Instantiates the Segment with data
+     * @param l Segment length
+     * @param w Segment width
+     * @param a Segment corner angle
+     * @param g Segment surface grip
+     */
+    public Segment(int l, int w, int a, int g){
         length = l;
         width = w;
         angle = a;
+        grip = g;
     }
 
     public int getLength() {
@@ -27,8 +38,13 @@ public class Segment{
         return angle;
     }
 
+    public int getGrip() {
+        return grip;
+    }
+
+    @Override
     public String toString(){
-        return ("Length: " + length + " Width: " + width + " Angle: " + angle);
+        return ("Length: " + length + " Width: " + width + " Angle: " + angle + " Surface grip: " + grip);
     }
 
 }
