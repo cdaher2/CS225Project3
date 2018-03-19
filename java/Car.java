00c5ff27014a2e077e768b5f2a68995085d3b4c1
waycoolway <benflorek@hotmail.com>
2018-03-19 12:29:03 -0400

    Update Car.java
    Changed local variables to class variables
    Added getters and setters
    Added basic calculation for top speed
    Added file reading for attributes

--- java/Car.java
+++ java/Car.java
@@ -1,43 +1,144 @@
 package RacingSimulator;
 
 import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
 
 public class Car {
 
-    int driveWheels;
-    int tireGrip;
-    int engineMaxRPM;
-    int enginePeakTorque;
-    int currentEngineRPM;
-    int currentEngineTorque;
-    int weight;
-    int downforceFromAero;
-    int engineTemp;
-    int damage;
-    int frontSuspensionStiffness;
-    int rearSuspensionStiffness;
-    Driver driver;
-    String makeAndModel;
-    File image, dataFile;
-    double[][] coordinates;
+    private int driveWheels;
+    private int tireGrip;
+    private int engineMaxRPM;
+    private int enginePeakTorque;
+    private int currentEngineRPM;
+    private int currentEngineTorque;
+    private int weight;
+    private int downforceFromAero;
+    private int engineTemp;
+    private int damage;
+    private int frontSuspensionStiffness;
+    private int rearSuspensionStiffness;
+    private Driver driver;
+    private String makeAndModel;
+    private File image, dataFile;
+    private double[][] coordinates;
 
-
-    public Car(String f){
-        dataFile = new File(f);
+    public Car(){
+        dataFile = new File("file.txt");
         readDataFromFile();
     }
-
-
+    
+    
     private void readDataFromFile(){
-
+      try{
+        Scanner scan = new Scanner(dataFile);
+        while(scan.hasNextLine()){
+          setDriveWheels(scan.nextInt());
+          setTireGrip(scan.nextInt());
+          setEngineMaxRPM(scan.nextInt());
+          setEnginePeakTorque(scan.nextInt());
+          setCurrentEngineRPM(scan.nextInt());
+          setCurrentEngineTorque(scan.nextInt());
+          setWeight(scan.nextInt());
+          setDownforceFromAero(scan.nextInt());
+          setEngineTemp(scan.nextInt());
+          setDamage(scan.nextInt());
+          setFrontSuspensionStiffness(scan.nextInt());
+          setRearSuspensionStiffness(scan.nextInt());
+        }
+        scan.close();
+      }
+      catch(FileNotFoundException e){
+        e.printStackTrace();}
     }
-
     public double calculateTopSpeedInCurrentConditions(){
-        return 0;
+      return (driveWheels+tireGrip+engineMaxRPM+enginePeakTorque+currentEngineRPM+currentEngineTorque+
+              weight+downforceFromAero+engineTemp+damage+frontSuspensionStiffness+rearSuspensionStiffness);
+      
+    } 
+    public int getDriveWheels(){
+        return driveWheels;
+    }
+    public int getTireGrip(){
+        return tireGrip;
+    }
+    public int getEngineMaxRPM(){
+        return engineMaxRPM;
+    }
+    public int getEnginePeakTorque(){
+        return enginePeakTorque;
+    }
+    public int getCurrentEngineRPM(){
+        return currentEngineRPM;
+    }
+    public int getCurrentEngineTorque(){
+        return currentEngineTorque;
+    }
+    public int getWeight(){
+        return weight;
+    }
+    public int getDownforceFromAero(){
+        return downforceFromAero;
+    }
+    public int getEngineTemp(){
+        return engineTemp;
+    }
+    public int getDamage(){
+        return damage;
+    }
+    public int getFrontSuspensionStiffness(){
+        return frontSuspensionStiffness;
+    }
+    public int getRearSuspensionStiffness(){
+        return rearSuspensionStiffness;
+    }
+    public Driver getDriver(){
+        return driver;
     }
 
-    public void findBestLine(){
-
+    public String getMakeAndModel(){
+        return makeAndModel;
     }
-
+    public void setDriveWheels(int dw){
+        driveWheels = dw;
+    }
+    public void setTireGrip(int tg){
+        tireGrip = tg;
+    }
+    public void setEngineMaxRPM(int emRPM){
+        engineMaxRPM = emRPM;
+    }
+    public void setEnginePeakTorque(int ept){
+        enginePeakTorque = ept;
+    }
+    public void setCurrentEngineRPM(int ceRPM){
+        currentEngineRPM = ceRPM;
+    }
+    public void setCurrentEngineTorque(int cet){
+        currentEngineTorque = cet;
+    }
+    public void setWeight(int w){
+        weight = w;
+    }
+    public void setDownforceFromAero(int dfa){
+        downforceFromAero = dfa;
+    }
+    public void setEngineTemp(int et){
+        engineTemp = et;
+    }
+    public void setDamage(int d){
+        damage = d;
+    }
+    public void setFrontSuspensionStiffness(int fss){
+        frontSuspensionStiffness = fss;
+    }
+    public void setRearSuspensionStiffness(int rss){
+        rearSuspensionStiffness = rss;
+    }
+    public void setDriver(Driver d){
+        driver = d;
+    }
+    public void setMakeAndModel(String mam){
+        makeAndModel = mam;
+    }
 }
