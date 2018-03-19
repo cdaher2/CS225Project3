ec8aabf3afc466a9a543018298aa8f2137e65f1b
cdaher2 <31801899+cdaher2@users.noreply.github.com>
2018-03-19 19:53:23 +0000

    Fixed testing methods

--- java/TrackTest.java
+++ java/TrackTest.java
@@ -8,5 +8,11 @@ public class TrackTest {
         Track t = new Track(new File("testtrack.txt"));
         Thread.sleep(100);
         System.out.println(t.toString());
+        Segment s;
+        for (int i = 0; i < 40; i++){
+            s = t.getNextSegment();
+            System.out.println(s.toString());
+
+        }
     }
 }
